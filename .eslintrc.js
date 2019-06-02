8bc2dfe3386bc46a591c20ae7499f0fb08ec1a3f
masa-suzu <11026517+masa-suzu@users.noreply.github.com>
2019-06-03 02:29:45 +0900

    Init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+    "jquery" : true
+  },
+  "rules": {
+    "semi": [1, "always"],
+    "quotes": [2, "double"],
+    "no-var": 1,
+    "indent": [2, 2]
+  }
+}
--- es6/ch02/main.js
+++ es6/ch02/main.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
-    'use strict';
+$(document).ready(function () {
+  "use strict";
 
-    console.log("jQuery has been loaded.");
-    paper.install(window);
-    paper.setup(document.getElementById('mainCanvas'));
+  console.log("jQuery has been loaded.");
+  paper.install(window);
+  paper.setup(document.getElementById("mainCanvas"));
 
-    let c = Shape.Circle(200,200,50);
-    c.fillColor = 'green';
+  let c = Shape.Circle(200, 200, 50);
+  c.fillColor = "green";
 
-    let tool = new Tool();
-    tool.onMouseDown = function(event){
-        let c = Shape.Circle(event.point.x,event.point.y,20);
-        c.fillColor = 'black';
-    };
-    
-    paper.view.draw();
+  let tool = new Tool();
+  tool.onMouseDown = function (event) {
+    let c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = "black";
+  };
 
-    console.log("main.js is loaded here.");
+  paper.view.draw();
+
+  console.log("main.js is loaded here.");
 });
